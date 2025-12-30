e32fddbb12b03b984c5c1ea78728f8166f968be5
Samuel Alemayehu Tadesse <Samuel.Alemayehu@ethio.local>
2025-12-30 20:52:48 +0300

    Prices Difference

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,12 +1,8 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../config/firebase');
+const { initializeFirebase } = require('../config/firebase');
 
 // Initialize Firebase Admin if not already done
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-}
+initializeFirebase();
 
 const isAdmin = async (req, res, next) => {
   try {
